b1a2e91dfe8c7d6a97b3dcde592fef2b1da802d4
Inti <yeseniase@outlook.it>
2019-10-03 22:01:17 -0500

    ADD: HOOCKS (State & Effect)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,37 +7,40 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className='App'>
-        <Header />
-        <Search />
+const App = () => {
+    const [ videos, setVideos ] = useState([]);
 
-        <Categories title='Mi lista'>
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+    useEffect(() =>{
+        fetch('http://localhost:3000/initialState')
+        .then(response => response.json())
+        .then(data => setVideos(data));
+    }, []);
 
-        <Categories title='Tendencias'>
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+    return (
+        <div className='App'>
+            <Header />
+            <Search />
 
-        <Categories title='Favoritos'>
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+            <Categories title='Mi lista'>
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+
+            <Categories title='Tendencias'>
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+
+            <Categories title='Favoritos'>
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+    )
+};
 
 export default App;
