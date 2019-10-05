a644a6f30e78790677d9a91ddc24a395092cb99e
Inti <yeseniase@outlook.it>
2019-10-05 00:01:01 -0500

    UPDATE: autor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => (
         <a href="/">Terminos de uso</a>
         <a href="/">Declaración de privacidad</a>
         <a href="/">Centro de ayuda</a>
+        <a href="/" className="footer-author"> Con ♡ por <strong>IntiDev</strong> </a>
     </footer>
 );
 
