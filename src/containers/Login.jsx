dcd2730e891b7573da6918683e5f9a21d7660866
Inti <yeseniase@outlook.it>
2019-10-15 22:14:06 -0500

    ADD: formulario login con Redux

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss'
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
+const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+    
+  };
+
+  return (
     <section className="login">
     <section className="login__container">
       <h2>Inicia sesión</h2>
-      <form className="login__container--form">
-        <input className="input" type="text" placeholder="Correo"/>
-        <input className="input" type="password" placeholder="Contraseña"/>
+      <form className="login__container--form" onSubmit={ handleSubmit }>
+        <input
+          name="email"
+          className="input"
+          type="text"
+          placeholder="Correo"
+          onChange={ handleInput }
+        />
+        <input
+          name="password"
+          className="input"
+          type="password"
+          placeholder="Contraseña"
+          onChange={ handleInput }
+        />
         <button className="button">Iniciar sesión</button>
         <div className="login__container--remember-me">
           <label>
-            <input type="checkbox" id="cbox1" value="first_checkbox"/>Recuérdame
+            <input
+              type="checkbox"
+              id="cbox1"
+              value="first_checkbox"
+            />
+            Recuérdame
           </label>
           <a href="/">Olvidé mi contraseña</a>
         </div>
@@ -31,6 +66,7 @@ const Login = () => (
       </p>
     </section>
   </section>
-);
+  );
+}
 
 export default Login;
