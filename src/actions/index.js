a1d0b73386472b7997c3c707e9baadacac1521fc
Inti <yeseniase@outlook.it>
2019-10-20 17:50:31 -0500

    ADD: player

--- src/actions/index.js
+++ src/actions/index.js
@@ -32,3 +32,10 @@ export const registerRequest = payload => (
         payload,
     }
 );
+
+export const getVideoSource = payload => (
+    {
+        type: 'GET_VIDEO_SOURCE',
+        payload,
+    }
+);
--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { setFavorite, deleteFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss'
 import playIcon from '../assets/static/play-icon.png';
@@ -28,11 +29,13 @@ const CarouselItem = (props) => {
             />
             <div className='carousel-item__details'>
                 <div>
-                    <img
-                        className='carousel-item__details--img'
-                        src={ playIcon }
-                        alt='Play Icon'
-                    />
+                    <Link to={`/player/${id}`}>
+                        <img
+                            className='carousel-item__details--img'
+                            src={ playIcon }
+                            alt='Play Icon'
+                        />
+                    </Link>
                     { isList ?
                         <img
                         className='carousel-item__details--img'
