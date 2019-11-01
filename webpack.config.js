2c110622055c8ec4b2bc176b4176dfd0a2a22165
Inti <yeseniase@outlook.it>
2019-11-01 13:27:54 -0600

    ADD: Plugins y vendor file

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,26 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimizations: {
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vedors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: 'assets/vendor.js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some((chunk) => chunk.name !== 'vendor' && /[\\/]node_modules[\\/]/.test(name));
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
@@ -66,6 +86,7 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
         postcss: [
